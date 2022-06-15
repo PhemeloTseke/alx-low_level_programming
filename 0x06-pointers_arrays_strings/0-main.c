5b620227a44a769259fc7730f232bfc509315cd1
PhemeloTseke <phemelotseke2@gmail.com>
2022-06-15 12:28:22 -0700

    commiting changes

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - creating a main dot h file
+ * main - check the code
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 char s1[98] = "Hello ";
 char s2[] = "World!\n";
 char *ptr;
 
-printf("%s\n", s1);
+printf("%s\n", s1); 
 printf("%s", s2);
 ptr = _strcat(s1, s2);
 printf("%s", s1);
